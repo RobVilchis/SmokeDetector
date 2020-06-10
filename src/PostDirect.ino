33388f0cc6411b95b6d68b55a120d5b52c7b5a34
Alejandro Vilchis <alexrv4299@gmail.com>
2020-06-09 22:17:54 -0500

    Update

--- src/PostDirect.ino
+++ src/PostDirect.ino
@@ -4,52 +4,51 @@
 //#include "SoftwareSerial.h"
 //SoftwareSerial Serial1(6, 7); // RX, TX
 
+String URL = "https://e6mjmurtkd.execute-api.us-east-2.amazonaws.com/default/nova3d-form-to-google-sheets";
 
- 
-void setup() {
- 
-  Serial.begin(115200);                                  //Serial connection
-  WiFi.begin("Vilchis", "0047621814");   //WiFi connection
-  
-  while (WiFi.status() != WL_CONNECTED) 
-  {  //Wait for the WiFI connection completion
+void setup()
+{
+
+  Serial.begin(115200);                //Serial connection
+  WiFi.begin("Vilchis", "0047621814"); //WiFi connection
+
+  while (WiFi.status() != WL_CONNECTED)
+  { //Wait for the WiFI connection completion
     delay(500);
     Serial.println("Waiting for connection");
   }
-  
 }
- 
-void loop() {
-  /*StaticJsonDocument<200> doc;
+
+void loop()
+{
+  StaticJsonDocument<200> doc;
   doc["correas"] = 4;
   doc["whatsapp"] = 2;
   doc["caretas_ad"] = 0;
   doc["caretas_ni"] = 0;
-  doc["nombre"] = "pruebaArduino";*/
+  doc["nombre"] = "pruebaArduino";
   String output;
-  //serializeJsonPretty(doc, output);
-  if(WiFi.status()== WL_CONNECTED){   //Check WiFi connection status
-  
-   HTTPClient http;    //Declare object of class HTTPClient
-  
-   http.begin("http://192.168.1.82:8080");      //Specify request destination
-   http.addHeader("Content-Type", "application/json");  //Specify content-type header
-  
-   int httpCode = http.POST(output);   //Send the request
-   String payload = http.getString();                  //Get the response payload
-  
-   Serial.println(httpCode);   //Print HTTP return code
-   Serial.println(payload);    //Print request response payload
-  
-   http.end();  //Close connection
-  
+  serializeJsonPretty(doc, output);
+  if (WiFi.status() == WL_CONNECTED)
+  {                  //Check WiFi connection status
+    HTTPClient http; //Declare object of class HTTPClient
+
+    http.begin(URL);                                    //Specify request destination
+    http.addHeader("Content-Type", "application/json"); //Specify content-type header
+
+    int httpCode = http.POST(output); //Send the request
+
+    String payload = http.getString(); //Get the response payload
+    Serial.println(payload);           //Print request response payload
+
+    Serial.println(httpCode); //Print HTTP return code
+
+    http.end(); //Close connection
   }
-  else{
-  
-    Serial.println("Error in WiFi connection");   
-  
+  else
+  {
+    Serial.println("Error in WiFi connection");
   }
-  
-  delay(30000);  //Send a request every 30 seconds
- 
+
+  delay(5000); //Send a request every 30 seconds
 }
